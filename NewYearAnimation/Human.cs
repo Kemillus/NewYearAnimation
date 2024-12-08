1584396b456c8a2389848e540019c48fe4de79d4
Kemillus <b.a.a.ss@mail.ru>
2024-12-08 17:26:04 +0300

    process1.1

---
+++ NewYearAnimation/Human.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewYearAnimation
+{
+    internal class Human
+    {
+        public int X {  get; set; }
+        public int Y { get; set; }
+        public int Size { get; set; }
+        public int Step { get; set; }
+        public Image Image { get; set; }
+
+        Human(int x, int y, int size, Image image) 
+        {
+            X = x;
+            Y = y;
+            Size = size;
+            Image = image;
+        }
+    }
+}
